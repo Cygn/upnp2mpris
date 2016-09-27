183ef9ff97c132d85d53f1f5c768181f621a1b9b
cygn <sinan@lamad.net>
2016-09-27 13:59:28 +0200

    Added logging & code cleanup

--- upnp2mpris.py
+++ upnp2mpris.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Sinan Haliyo <sinan@haliyo.net>
+#
+
 
 import gobject
+
+import logging
 
 import dbus
 import dbus.service
@@ -9,12 +29,6 @@ import dbus.mainloop.glib
 
 global manager
 manager = None
-
-global dleyna_object 
-dleyna_object = None
-
-global bus
-bus = None
 
 global Renderers
 Renderers=dict()
@@ -52,9 +66,8 @@ class RenderObject(dbus.service.Object):
         self.__playerIF = get_interface(object_path, PLAYER_IF_NAME)
         self.__pushhostIF = get_interface(object_path, PUSH_HOST_IF_NAME)
         self.__deviceIF = get_interface(object_path, DEVICE_IF_NAME)
-        
-        rendererName = self.__propsIF.Get('com.intel.dLeynaRenderer.RendererDevice','ModelName')
-        dbus_Name = "org.mpris.MediaPlayer2."+rendererName       
+        self.rendererName = self.__propsIF.Get('com.intel.dLeynaRenderer.RendererDevice','ModelName')
+        dbus_Name = "org.mpris.MediaPlayer2."+self.rendererName       
         bus_name = dbus.service.BusName(dbus_Name,bus)
         dbus.service.Object.__init__(self, bus_name, '/org/mpris/MediaPlayer2')
         
@@ -192,50 +205,52 @@ class RenderObject(dbus.service.Object):
         self.__pushhostIF.RemoveFile(path)
 
 
-def init_dleyna():
+def init_dleyna(bus):
+    
     bus.start_service_by_name('com.intel.dleyna-renderer')
-    global dleyna_object
+    
     dleyna_object = bus.get_object('com.intel.dleyna-renderer','/com/intel/dLeynaRenderer')
     global manager
     manager = dbus.Interface(dleyna_object,dbus_interface='com.intel.dLeynaRenderer.Manager')
-    try:
-        getRenderers()
-    except:
-        pass
 
 
 def makeRenderers(*args, **kwargs):
     obj = manager.GetRenderers()
     for i in obj:
-        if not i in Renderers.keys():
-            Renderers[i]=createDbusObject(i)
+        if i not in Renderers.keys():
+            Renderers[i]=createMprisObject(i)
 
 def checkRenderers(*args, **kwargs):
     obj = manager.GetRenderers()
     for i in Renderers.keys():
         if i not in obj:
             Renderers[i].remove_from_connection()
+            logger.info("Lost renderer %s at %s",Renderers[i].rendererName,i)
             del Renderers[i]
         
 
-def createDbusObject(path):
-
-    #renderer = dbus.proxies.ProxyObject(conn=bus, bus_name='org.mpris.MediaPlayer2.upnp',object_path='/org/mpris/MediaPlayer2')
-    #print "Renderer_before=",renderer
-    #renderer = bus.get_object('com.intel.dleyna-renderer',path)
+def createMprisObject(path):
     renderer = RenderObject(path)
+    logger.info("New renderer %s at %s", renderer.rendererName, path)
+    
     return renderer
 
 
 
 if __name__ == '__main__':
+    
+    log_format = '%(asctime)s %(module)s %(levelname)s: %(message)s'
+    log_level = logging.INFO
+    logging.basicConfig(format=log_format, level=log_level)
+    logger = logging.getLogger('upnp2mpris')
+
 
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     global bus
     bus = dbus.SessionBus()
 
-    dleyna_obj = init_dleyna()
+    init_dleyna(bus)
 
     bus.add_signal_receiver(makeRenderers, dbus_interface = 'com.intel.dLeynaRenderer.Manager', signal_name = "FoundRenderer")
     bus.add_signal_receiver(checkRenderers, dbus_interface = 'com.intel.dLeynaRenderer.Manager', signal_name = "LostRenderer")
